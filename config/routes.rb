2aab54f96458bbdcfe4e6542d77530fd5b1d3cc8
Caio Victor <caiovdamorim@gmail.com>
2021-08-30 22:36:41 -0300

    Primeiras alterações

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+
+  # Para mais detalhes da DSL disponível para esse arquivo, veja https://guides.rubyonrails.org/routing.html
 end
