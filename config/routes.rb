2000f9a00b6394b7c15124c17077d09c2814b37f
Caio Victor <caiovdamorim@gmail.com>
2021-08-31 10:35:55 -0300

    Atualização até a parte 6 do exercício

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  root "articles#index"
 
-  # Para mais detalhes da DSL disponível para esse arquivo, veja https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
 end
